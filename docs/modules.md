7c0f5729a32d01efe05fd3a878c91304eba9ed55
Pawel <pawel@bartczak.tech>
2024-12-03 19:34:48 +0000

    Added module with exceptions

--- docs/modules.md
+++ docs/modules.md
@@ -1,2 +1,3 @@
 ::: trivia_game.trivia_api
 :::trivia_game.models
+:::trivia_game.exceptions"
---
+++ trivia_game/exceptions.py
@@ -0,0 +1,31 @@
+"""Module for Trivia API exceptions"""
+
+
+class TriviaAPIError(Exception):
+    """Base exception for Trivia API errors"""
+
+    pass
+
+
+class NoResultsError(TriviaAPIError):
+    """Not enough questions available"""
+
+    pass
+
+
+class InvalidParameterError(TriviaAPIError):
+    """Invalid parameters provided"""
+
+    pass
+
+
+class TokenError(TriviaAPIError):
+    """Token-related errors"""
+
+    pass
+
+
+class RateLimitError(TriviaAPIError):
+    """Rate limit exceeded"""
+
+    pass
